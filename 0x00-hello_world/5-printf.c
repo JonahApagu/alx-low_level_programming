5df71e72c44b828480698d424ead9bf20fefc5e1
Jonah Apagu <jonahapagu54@gmail.com>
2022-07-04 07:01:19 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * use the printf function
+ * print out the words "with proper grammar, but the outcome is a piece of art,"
+ * Use the main finction
+ * returns 0
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+ return (o)
+}
